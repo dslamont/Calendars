fd82d041a2d97f4ab6c9429bd1af75edbab7a30e
Don Lamont <5798888+dslamont@users.noreply.github.com>
2020-06-28 17:45:01 +0100

    Admin tidy up

--- Calendars/Calendar.cs
+++ Calendars/Calendar.cs
@@ -1,20 +1,14 @@
-using System;
-using System.IO;
-using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
 using Microsoft.WindowsAzure.Storage;
-using Microsoft.WindowsAzure.Storage.Blob;
-using Microsoft.Azure;
 using Microsoft.WindowsAzure.Storage.Auth;
-using Microsoft.Extensions.Configuration;
-using System.Diagnostics;
-using System.Reflection;
-using System.Net;
+using Microsoft.WindowsAzure.Storage.Blob;
+using System;
+using System.IO;
+using System.Threading.Tasks;
 
 namespace Calendars
 {
@@ -26,7 +20,7 @@ namespace Calendars
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = "Calendar/{name}.ics")] HttpRequest req, string name,
             ILogger log)
         {
-            log.LogInformation($"{nameof(Calendar)} trigger function processed a request.");
+            log.LogInformation($"{nameof(Calendar)} trigger function processed a request."); 
 
             string account_name = Environment.GetEnvironmentVariable("AccountName", EnvironmentVariableTarget.Process); 
             string account_key = Environment.GetEnvironmentVariable("AccountKey", EnvironmentVariableTarget.Process);
